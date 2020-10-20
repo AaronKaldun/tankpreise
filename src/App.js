adc618e6ed584650b06d704d2fae09dd4abdae00
AaronKaldun <aaron.kaldun@gmail.com>
2020-10-20 20:44:53 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import LocationSearch from "./components/LocationSearch";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [preise, setPreise] = useState([]);
+
+    const renderedResults = preise.map((preis) => {
+        return (
+            <div className="item" key={preis.id}>
+                <div className="right floated content">
+                    {preis.price.toFixed(2)} <b>€</b>
+                </div>
+                <div className="content">
+                    <div className="header">{preis.brand}</div>
+                    {`${preis.street}, ${preis.houseNumber} - ${preis.place} | Entfernung: ${preis.dist} km`}
+                </div>
+            </div>
+        );
+    });
+
+    return (
+        <div className="ui container">
+            <LocationSearch onPreiseChange={setPreise} />
+            <div className="ui inverted segment">
+                <div className="ui inverted relaxed divided list">
+                    {renderedResults}
+                </div>
+            </div>
+        </div>
+    );
+};
 
 export default App;
---
+++ src/components/LocationSearch.js
@@ -0,0 +1,95 @@
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import Dropdown from "./Dropdown";
+import { Slider } from "react-semantic-ui-range";
+//import "semantic-ui-css/semantic.min.css";
+
+const LocationSearch = ({ onPreiseChange }) => {
+    const API_KEY = "d8149be7-4874-1428-b259-da5421cf17aa";
+    const options = [
+        {
+            label: "Diesel",
+            value: "diesel",
+        },
+        {
+            label: "Super",
+            value: "e5",
+        },
+        {
+            label: "Super E10",
+            value: "e10",
+        },
+    ];
+
+    const [lat, setLat] = useState(null);
+    const [lng, setLng] = useState(null);
+    const [err, setErr] = useState("");
+    const [fuel, setFuel] = useState(options[0]);
+
+    const [value, setValue] = useState(5);
+
+    const settings = {
+        start: 5,
+        min: 0,
+        max: 25,
+        step: 5,
+        onChange: (value) => {
+            setValue(value);
+        },
+    };
+
+    const handleValueChange = (e) => {
+        let value = parseInt(e.target.value);
+        if (!value) {
+            value = 0;
+        }
+        setValue(e.target.value);
+    };
+
+    useEffect(() => {
+        //$(".ui.slider").slider();
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                setLat(position.coords.latitude);
+                setLng(position.coords.longitude);
+            },
+            (err) => setErr(err.message)
+        );
+    }, []);
+
+    useEffect(() => {
+        if (lat && lng && fuel)
+            axios
+                .get("https://creativecommons.tankerkoenig.de/json/list.php", {
+                    params: {
+                        lat: lat,
+                        lng: lng,
+                        rad: value,
+                        type: fuel.value,
+                        sort: "price",
+                        apikey: API_KEY,
+                    },
+                })
+                .then(({ data }) => {
+                    onPreiseChange(data.stations);
+                });
+    }, [fuel, lat, lng, onPreiseChange, value]);
+
+    return (
+        <div>
+            <div className="ui form">
+                <Dropdown
+                    options={options}
+                    label="Treibstoff wählen:"
+                    selected={fuel}
+                    onSelectedChange={setFuel}
+                />
+                <Slider value={value} color="blue" settings={settings} />
+                <h3 className="header">Umkreis: {value} km</h3>
+                {err ? err : ""}
+            </div>
+        </div>
+    );
+};
+
+export default LocationSearch;
